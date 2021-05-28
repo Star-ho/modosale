c260201f427ba348ce68b7aa9bd307bb3b0109d2
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-05-28 16:14:06 +0900

    db연동

--- dbtest.js
+++ dbtest.js
@@ -5,35 +5,45 @@ port     :  3306,
 user     : 'root',
 password : 'root',
 database : 'menu',
-connectionLimit:4
 });
 
 (async ()=>{
 
     let connect= await pool.getConnection(conn =>conn)
+    let data = await connect.query('select * from menu where brandName="멕시카나";');
+    console.log(data[0][0])
+    
+    connect.release()
+//데이터 넣기
     // await connect.query('create table Menu(brandName varchar(100), imageName varchar(100),category varchar(100),PRIMARY KEY (brandName));');
     // connect.release()
 
-    var fs = require('fs');
-    fs.readFile('menufile.txt', 'utf8', async function(err, data){
-        data=data.split('\r\n').map(v=>v.split('&&'))
-        for(let i=0;i<data.length;i++){
-            if(data[i][0].indexOf('//')!=-1){
-                let temp=data[i][0].split('//')
-                data[i][0]=temp[0]
-                for(let j=1;j<temp.length;j++){
-                    data.push([temp[j],data[i][1],data[i][2]])   
-                }
+    // var fs = require('fs');
+    // fs.readFile('menufile.txt', 'utf8', async function(err, data){
+    //     data=data.split('\r\n').map(v=>v.split('&&'))
+    //     for(let i=0;i<data.length;i++){
+    //         if(data[i][0].indexOf('//')!=-1){
+    //             let temp=data[i][0].split('//')
+    //             data[i][0]=temp[0]
+    //             for(let j=1;j<temp.length;j++){
+    //                 data.push([temp[j],data[i][1],data[i][2]])   
+    //             }
                 
-            }
-        }
-        for(let i=0;i<data.length;i++){
-            let temp = await connect.query(`insert into Menu(brandName,imageName,category) values('${data[i][0]}','${data[i][1]}','${data[i][2]}')`);    
-            console.log(temp)
-            connect.release()
-        }
-    })
+    //         }
+    //     }
+    //     for(let i=0;i<data.length;i++){
+    //         let temp = await connect.query(`insert into Menu(brandName,imageName,category) values('${data[i][0]}','${data[i][1]}','${data[i][2]}')`);    
+    //         console.log(temp)
+    //         connect.release()
+    //     }
+    // })
 
+
+
+
+
+
+///기초
 
 // connection.query('insert into Test(brandName,imageName,category) values(1,1,1)', (error, rows, fields) => {
 //   if (error) throw error;
@@ -42,5 +52,6 @@ connectionLimit:4
 // connection.query('create table Test(brandName varchar(100), imageName varchar(100),category varchar(100));', (error, rows, fields) => {
 //     if (error) throw error;
 //   });
+    connect.destroy()
 
 })()
--- index.js
+++ index.js
@@ -3,11 +3,43 @@
 import {getDataArray} from './yogiyo' 
 import {getData} from './baemin.js'
 
-let data;
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+host     : 'localhost',
+port     :  3306,
+user     : 'root',
+password : 'root',
+database : 'menu',
+connectionLimit:4
+});
+
+let data={};
+
 (async ()=>{
-data={yogiyo:await getDataArray()};
-Object.assign(data,{baemin:await getData()})
-console.log(data)
+   let connect= await pool.getConnection(conn =>conn)
+   
+   for(let i of Object.entries(await getDataArray())){
+      let SqlRes = await connect.query(`select * from menu where brandName="${i[0]}";`);
+      if(SqlRes[0][0]){
+         Object.assign(data,{ [i[0]] : [ "yogiyo",SqlRes[0][0].imageName, SqlRes[0][0].category, +i[1] ] } )
+      }else{
+         Object.assign(data,{ [i[0]] : [ "yogiyo", "없음", "없음", +i[1] ] } )
+      }
+      connect.release()
+   }
+   for(let i of Object.entries(await getData())){
+      let SqlRes = await connect.query(`select * from menu where brandName="${i[0]}";`);
+      console.log(`select * from menu where brandName="${i[0]}";`)
+      if(SqlRes[0][0]){
+         Object.assign(data,{ [i[0]] : [ "baemin", SqlRes[0][0].imageName, SqlRes[0][0].category, +i[1] ] } )
+      }else{
+         Object.assign(data,{ [i[0]] : [ "baemin", "noimage.png", "기타", +i[1] ] } )
+      }
+      connect.release()
+   }
+   //Object.assign(data,{baemin:})
+   connect.destroy()
+   //console.log(data)
 })()
 var http = require("http");
 
