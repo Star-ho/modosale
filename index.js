c81234cd77dfea2bb9ffe5ffa7ab3c2be91d2ef9
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-06-23 00:03:11 +0900

    setData split baemin, coupang

--- index.js
+++ index.js
@@ -68,32 +68,6 @@ async function setData(){
          connect.release()
       }
       data={}
-      for(let i of ( await wemefReadData() ) ){
-         let SqlRes = await connect.query(`select * from Menu where brandName="${i[0]}";`);
-         if(SqlRes[0][0]){
-            if(SqlRes[0][0].category=="치킨"||SqlRes[0][0].category=="피자"||SqlRes[0][0].category=="한식"||SqlRes[0][0].category=="양식"){
-               await connect.query(`insert into data(app,brand,price,img,category,uri) values('wemef','${i[0]}','${+i[1]}','${SqlRes[0][0].imageName}','${SqlRes[0][0].category}','${i[2]}')`);    
-            }else{
-               await connect.query(`insert into data(app,brand,price,img,category,uri) values('wemef','${i[0]}','${+i[1]}','${SqlRes[0][0].imageName}','기타','${i[2]}')`);    
-            }
-         }else{
-            await connect.query(`insert into data(app,brand,price,img,category,uri) values('wemef','${i[0]}','${+i[1]}','없음','기타','${i[2]}')`);    
-         }
-         //console.log(i)
-      }
-      for(let i of ( await coupangReadData() ) ){
-         let SqlRes = await connect.query(`select * from Menu where brandName="${i[0]}";`);
-         if(SqlRes[0][0]){
-            if(SqlRes[0][0].category=="치킨"||SqlRes[0][0].category=="피자"||SqlRes[0][0].category=="한식"||SqlRes[0][0].category=="양식"){
-               await connect.query(`insert into data(app,brand,price,img,category,uri) values('coupang','${i[0]}','${+i[1]}','${SqlRes[0][0].imageName}','${SqlRes[0][0].category}','${i[2]}')`);    
-            }else{
-               await connect.query(`insert into data(app,brand,price,img,category,uri) values('coupang','${i[0]}','${+i[1]}','${SqlRes[0][0].imageName}','기타','${i[2]}')`);    
-            }
-         }else{
-            await connect.query(`insert into data(app,brand,price,img,category,uri) values('coupang','${i[0]}','${+i[1]}','없음','기타','${i[2]}')`);    
-         }
-         //console.log(i)
-      }
    }catch(e){
       console.log(e)
    }finally{
