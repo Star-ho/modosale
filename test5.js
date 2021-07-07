82d9f196c9720af093e5b4bf0ded83ff05b411d9
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-07-07 19:25:17 +0900

    chagne Coupang itemlist

--- test5.js
+++ test5.js
@@ -1,5 +1,15 @@
 const { insertAfter } = require('cheerio/lib/api/manipulation');
 var qs = require('querystring');
-console.log('coupangeats://SearchResult?keyword='+qs.escape('바른치킨')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('매드포갈릭')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('애슐리')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('제일제면소')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('빕스')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('더플레이스')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('피자몰')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('카페리피')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('잠바주스')+'&sourceType=Promotion')
+console.log('coupangeats://SearchResult?keyword='+qs.escape('커피앳웍스')+'&sourceType=Promotion')
+
+
 
 
