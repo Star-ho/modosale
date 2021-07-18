fa10cee0b1b3b02e3f87431b4876de13c4d479f3
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-07-19 00:19:56 +0900

    change unifiedMenu

--- InsertUnifiedName.js
+++ InsertUnifiedName.js
@@ -23,11 +23,9 @@ const pool = mysql.createPool({
     for(let i=0;i<data.length;i++){
         let temp = (await connect.query(`select * from UnifiedName where fakeName='${data[i][0]}'`))[0][0];    
         if(temp){
-            console.log(11)
-            console.log(data[i])
             for(let j=1;j<data[i].length;j++){
-                // let temp = await connect.query(`insert into UnifiedName(fakeName,realName) values('${data[i][j]}','${data[i][0]}')`);    
-                // console.log(data[i][j],data[i][0])
+                let temp = await connect.query(`insert into UnifiedName(fakeName,realName) values('${data[i][j]}','${data[i][0]}')`);    
+                console.log(data[i][j],data[i][0])
             }
         }
     }
