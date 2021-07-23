f5d93e7ef91e82ce9285380df86db495904a340d
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-07-23 22:37:31 +0900

    test

--- testfolder/test5.js
+++ testfolder/test5.js
@@ -1,8 +1,5 @@
-//npx babel-node --presets @babel/env test5.js
-
-import {wemefReadData } from '../readfile.js'
-
-(async()=>{
-
-    console.log(await wemefReadData())
-})()
+let  moment = require('moment');
+require('moment-timezone'); 
+moment.tz.setDefault("Asia/Seoul"); 
+let a= moment().format('MMM').toUpperCase()+'_'
+console.log(a)
