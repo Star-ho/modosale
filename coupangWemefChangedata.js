ea244dff936ccfa8d7056ab3d38984b145fd3f2c
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-08-03 12:31:01 +0900

    add telegram alert

--- coupangWemefChangedata.js
+++ coupangWemefChangedata.js
@@ -1,6 +1,7 @@
 //npx babel-node --presets @babel/env coupangWemefChangedata.js
 
 import { getWemefData } from './imgWemef.js'
+import { telegramSendMessage } from './teleWebhook.js';
 
 export async function wemefReadData(){
     let fs = require('fs');
@@ -11,23 +12,32 @@ export async function wemefReadData(){
     readData=readData.split(EOL).map(v=>v.split('||'))
     // console.log(readData)
     // console.log(wemefData)
-    readData.forEach(v=>{
-        for(let i of wemefData){
-            if(i.id==v[2]){
-                if(Object.keys(res).includes(v[0])){
-                    // console.log(v)
+    //현재 존재하는값 정리
+    wemefData.forEach(v=>{
+        for(let i of readData){
+            if(v.id==i[2]){
+                if(Object.keys(res).includes(i[0])){
+                    if(+i[1]>+res[i[0]][0]){
+                        Object.assign(res,{ [i[0]] :[i[1],v.scheme] })    
+                    }
                     return
                 }
-                Object.assign(res,{ [v[0]] :[v[1],i.scheme] })    
+                Object.assign(res,{ [i[0]] :[i[1],v.scheme] })    
+                return
             }
         }
+        if(v.id&&v.id.includes('신규')==false){
+            telegramSendMessage(JSON.stringify(v))
+        }
     });
+
     // console.log(Object.keys(res).length)
     return res
 }
 
 // (async()=>{
-//     console.log(await wemefReadData())
+    // await wemefReadData()
+    // console.log(await wemefReadData())
 // })()
 
 export async function coupangReadData(){
